2a884b430fcaeebdac4054e387b7c989dc14e250
Ronald <rpalacioso12@gmail.com>
2022-04-27 08:55:23 -0300

    Actualizado

--- ServidorExpress/app.js
+++ ServidorExpress/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const Contenedor = require("./container");
+const contenedor = require("./container");
 const fs = require("fs");
 const app = express();
 const PORT = 8080;
 
-const PRODUCTS = new Contenedor("productos.json");
-
+const products = new contenedor("./productos.json");
+products.init();
 
 app.listen(process.env.PORT || PORT, () => {
   console.log("Server corriendo en el puerto 8080");
@@ -14,14 +14,30 @@ app.on("error", (error) => {
   console.log("Hubo un error");
 });
 
+const save = products.save({title:'Galaxis', price:'1200', thumbnail: 'movistar.com.ar', id:6 }) //modificar siempre para agregar nuevos productos
+    console.log (`Nuevo item con id ${save}`);
+    
+
 app.get("/", (request, response) => {
   response.send(`<h1 style='color:blue;'>Bienvenidos a Express</h1>`);
 });
 
-app.get("/products", (request, response) => {
-  response.send(PRODUCTS.getAll());
-});
-
-app.get("/productsRandom", (request, response) => {
-  response.send(PRODUCTS.getById(Math.floor(Math.random() * (PRODUCTS.countID - 1 + 1) + 1)));
-});
+app.get('/productos',(req, res) =>{
+  try{
+      const elementos = products.getAll()
+      res.send(` Los elementos del archivo son ${JSON.stringify(elementos)}`)
+  }
+  catch(err){
+      res.send(`Ocurrio un error al obtener los elementos: ${err}`)
+  }
+})
+app.get('/productoRandom',(req, res) =>{
+  const id = Math.floor(Math.random() * 4) + 1
+  try{
+      const elementoRandom = products.getById(id)
+      res.send(`El elemento con id ${id}, es ${JSON.stringify(elementoRandom)}`)
+  }
+  catch(err){
+      res.send(`Ocurrio un error al intentar obtener el elemento por id ${err}`)
+  }
+})
--- ServidorExpress/container.js
+++ ServidorExpress/container.js
@@ -1,59 +1,79 @@
-
-const fs = require('fs')
+const fs = require("fs");
 
 class container {
-    constructor(name) {
-        this.fileName = name
-        this.countID = 0
-        this.content = []
-    }
+  constructor(fileName) {
+    this.file = fileName;
+    this.countID = 0;
+    this.list = [];
+  }
 
-    async write() { //Método que escribe/sobreescribe: de este manera queda más limpio el código de los otros métodos
-        await fs.promises.writeFile(this.fileName, JSON.stringify(this.content, null, ''))
+  async init() {
+    try {
+      const data = await fs.promises.readFile(this.file); //no se si es esta promesa la que me genera el bucle.!
+      this.list = JSON.parse(data);
+      for (const element of this.list) {
+        if (element.id > this.countID) this.countID = element.id;
+      }
+    } catch (error) {
+      console.log("No se encontro el archivo!!, generando...");
     }
-
-    save(object) {
-        this.countID++ //Aumento la propiedad que va guardando el ID más alto
-        object["id"] = this.countID //Agrego la propiedad id al objeto pasado como parámetro
-        this.content.push(object) //Agrego el objeto al contenido(array)
-        this.write() //Agrego el objeto al archivo
-        return `El id del objeto añadido es ${this.countID}` //Retorna el ID (lo solicita la consigna)
+  }
+  async write() {
+    try {
+      const str = JSON.stringify(this.list);
+      await fs.promises.writeFile(this.file, str);
+    } catch (err) {
+      console.log(err);
     }
-
-    async getAll() { //Devuelve un array con los objetos presentes en el archivo
-        return this.content
+  }
+  async save(object) {
+    try {
+      this.countID++;
+      object["id"] = this.countID;
+      this.list.push(object);
+      await this.write();
+      return this.countID;
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    getById(id) { //Recibe un id y devuelve el objeto con ese id, o null si no está.
-        let result
-        if (this.content !== []) {
-            result = this.content.find(x => x.id === id)
-            if (result === undefined) {
-                result = null
-            }
-        } else {
-            result = 'El archivo está vacío'
-        }
-        return result
+  getById(id){
+    try{
+        let element = this.list.find(item => item.id == id)
+        return element != undefined ? element : null
+        
     }
-
-    deleteById(id) { //Elimina del archivo el objeto con el id buscado
-        let result
-        if (this.content !== []) {
-            let newContent = this.content.filter(x => x.id !== id)
-            this.content = newContent
-            this.write() //SobreEscribo el archivo
-            result = 'OK'
-        } else {
-            result = `El archivo está vacío`
-        }
-        return result
-    }
-
-    async deleteAll() { //Elimina todos los objetos presentes en el archivo.
-        this.content = this.content.splice(0, this.content.length)
-        this.write()
+    catch(err){
+        console.log(err)
     }
 }
+getAll(){
+        const elements = this.list
+        return elements
+}
 
-module.exports = container
+async deleteById(id){
+    try{
+        const index = this.list.findIndex((element) => element.id == id)
+        if(index != -1){
+            this.list.splice(index, 1)
+            await this.write()
+        }
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+async deleteAll(){
+    try{
+        this.list = []
+        await this.write()
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+}
+
+module.exports = container;
